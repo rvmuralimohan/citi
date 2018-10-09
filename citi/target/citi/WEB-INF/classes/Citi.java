b057e76201b1cd297e2ba399e88644b5d2d9236b
rvmuralimohan <mohanmurali.r@gmail.com>
2018-10-09 15:11:56 +0530

    citi project inital files

---
+++ citi/target/citi/WEB-INF/classes/Citi.java
@@ -0,0 +1,8 @@
+
+public class Citi {
+	  public static void main(String[] args) {
+	        // Prints "Hello, World" to the terminal window.
+	        System.out.println("Hello, World");
+	    }
+
+}
