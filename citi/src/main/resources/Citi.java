9a7180da59e54c0e540b85485a3e068624f1b771
rvmuralimohan <mohanmurali.r@gmail.com>
2018-10-10 13:30:25 +0530

    DFASDFSADFDSA

--- citi/src/main/resources/Citi.java
+++
@@ -1,8 +0,0 @@
-
-public class Citi {
-	  public static void main(String[] args) {
-	        // Prints "Hello, World" to the terminal window.
-	        System.out.println("Hello, World");
-	    }
-
-}
